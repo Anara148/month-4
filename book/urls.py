8ce4039d0ec74da1eae573876ebc8c545e932b52
Anara148 <anaraberenbaeva93@gmail.com>
2026-02-03 01:58:51 +0600

    hw_2

--- book/urls.py
+++ book/urls.py
@@ -5,5 +5,5 @@ from . import view
 urlpatterns = [
     path('quotes/', views.quotes_view),
     path('book_list/', view.book_list_view),
-    path('book_list/<int:id>/', view.book_detail_view,),
+    path('book_list/<int:id>/', view.book_detail_view),
 ]
