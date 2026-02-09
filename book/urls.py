a57ce12178040364c9528e5a96f51223a4753242
Anara148 <anaraberenbaeva93@gmail.com>
2026-02-09 19:40:02 +0600

    hw_4

--- book/urls.py
+++ book/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from . import views
-from . import view
+
 
 urlpatterns = [
     path('quotes/', views.quotes_view),
-    path('book_list/', view.book_list_view),
-    path('book_list/<int:id>/', view.book_detail_view),
-    path('book_list/<int:id>/delete', view.delete_book_view),
-    path('book_list/<int:id>/update', view.update_book_view),
-    path('create_book_list/', view.create_book_view),
+    path('book_list/', views.book_list_view),
+    path('book_list/<int:id>/', views.book_detail_view),
+    path('book_list/<int:id>/delete', views.delete_book_view),
+    path('book_list/<int:id>/update', views.update_book_view),
+    path('create_book_list/', views.create_book_view),
     
 
 ]
--- book/views.py
+++ book/views.py
@@ -1,5 +1,83 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from . import models, forms
+
+
+
+def update_book_view(request, id):
+     book_id = get_object_or_404(models.BookList, id=id)
+     if request.method == "POST":
+          form = forms.BookListForm(request.POST, instance=book_id)
+          if form.is_valid():
+               form.save()
+               return redirect('/book_list/')
+     else:
+          form = forms.BookListForm(instance=book_id)
+     return render(
+          request,
+          'update_book.html',
+          {
+               "form": form,
+               "book_id": book_id
+          }
+     )
+
+
+
+def delete_book_view(request, id):
+     book_id = get_object_or_404(models.BookList, id=id)
+     book_id.delete()
+     return redirect('/book_list/')
+
+
+
+
+def create_book_view(request):
+    if request.method == 'POST':
+        form = forms.BookListForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/book_list/')
+    else:
+            form = forms.BookListForm()
+    return render(
+         request,
+         'create_book.html',
+         {
+              "form": form
+         }
+    )
+
+
+
+def book_detail_view(request, id):
+    if request.method == 'GET':
+        book_detail_id = get_object_or_404(models.BookList, id=id)
+        return render(
+            request,
+            'book_detail.html',
+            {
+                "book_id": book_detail_id
+            }
+        )
+
+
+
+
+def book_list_view(request):
+    if request.method == 'GET':
+        book_list = models.BookList.objects.all()
+        return render(
+            request,
+            'book_list.html',
+            {
+                    "book_list": book_list
+            }
+        )
+    
+
+
+
 
 
 def quotes_view(request):
